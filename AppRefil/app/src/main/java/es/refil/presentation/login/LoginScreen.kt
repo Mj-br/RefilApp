de24f3eb0b8758755bf2c2965e161647bd6e2be5
Mj-br <98741174+Mj-br@users.noreply.github.com>
2023-05-11 22:32:27 +0200

    Update

--- AppRefil/app/src/main/java/es/refil/presentation/login/LoginScreen.kt
+++ AppRefil/app/src/main/java/es/refil/presentation/login/LoginScreen.kt
@@ -1,22 +1,89 @@
 package es.refil.login.ui
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import es.refil.R
 
 @Composable
 fun LoginScreen(
 /* TODO: Comentamos solo para tener la preview de la pantalla
 mainMarketViewModel: LoginViewModel*/
 ) {
-    //Creamos un center text
+    //Header
     Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Login Screen")
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background)
+    )
+    {
+        Image(
+            painter = painterResource(id = R.drawable.logox),
+            contentDescription = "Login_Image",
+            contentScale = ContentScale.Inside
+        )
+
+        //Body with textField
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+
+            ConstraintLayout {
+
+                val (surface, fab) = createRefs()
+
+                Surface(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(400.dp)
+                        .constrainAs(surface) {
+                            bottom.linkTo(parent.bottom)
+                        },
+                    color = Color.White,
+                    shape = RoundedCornerShape(
+                        topStartPercent = 8,
+                        topEndPercent = 8
+                    )
+                ) {
+                    //TODO: TextFields Correo, contraseña y boton
+
+                }
+
+                FloatingActionButton(
+                    modifier = Modifier
+                        .size(72.dp)
+                        .constrainAs(fab) {
+                            top.linkTo(surface.top, margin = (-36).dp)
+                            end.linkTo(surface.end, margin = 36.dp)
+                        },
+                    backgroundColor = MaterialTheme.colors.primary,
+                    onClick = { }
+                ) {
+                    Icon(
+                        modifier = Modifier.size(42.dp),
+                        imageVector = Icons.Default.ArrowForward,
+                        contentDescription = "Forward Icon",
+                        tint = Color.White
+                    )
+
+                }
+
+            }
+
+        }
     }
+
 }
