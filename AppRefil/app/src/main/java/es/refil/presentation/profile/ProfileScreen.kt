271fb52fb97029694c3ed0b232c3163e16ccbe20
Mj-br <98741174+Mj-br@users.noreply.github.com>
2023-05-24 19:57:06 +0200

    Cleaning Profile

--- AppRefil/app/src/main/java/es/refil/presentation/profile/ProfileScreen.kt
+++ AppRefil/app/src/main/java/es/refil/presentation/profile/ProfileScreen.kt
@@ -1,5 +1,6 @@
 package es.refil.presentation.profile
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -10,8 +11,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -21,14 +20,17 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import es.refil.R
 import es.refil.data.models.User
 import es.refil.navigation.Destinations
 import es.refil.presentation.auth.AuthViewModel
+import es.refil.ui.theme.AppRefilTheme
 import es.refil.ui.theme.spacing
 
 
@@ -61,6 +63,8 @@ fun ProfileScreen(
             color = MaterialTheme.colorScheme.onSurface
         )
 
+        Spacer(modifier = Modifier.size(25.dp))
+
         //User profile picture from Google
         Image(
             painter = painterResource(id = R.drawable.logo),
@@ -74,6 +78,7 @@ fun ProfileScreen(
 
         )
 
+        Spacer(modifier = Modifier.size(20.dp))
 
         if (userData?.name != null) {
             Text(
@@ -94,174 +99,158 @@ fun ProfileScreen(
 
         }
 
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentHeight()
-                .padding(spacing.medium)
+        Spacer(modifier = Modifier.padding(25.dp))
+
+        Column(modifier = Modifier.fillMaxWidth()
+            .wrapContentHeight()
+            .padding(spacing.medium)
         ) {
-            Row(
+            Column(
                 modifier = Modifier
                     .fillMaxWidth()
                     .wrapContentHeight()
-            ) {
-                Text(
-                    text = stringResource(id = R.string.name),
-                    style = MaterialTheme.typography.bodyLarge,
-                    modifier = Modifier.weight(0.3f),
-                    color = MaterialTheme.colorScheme.onSurface
-                )
-
-                Text(
-                    text = viewModel?.currentUser?.displayName ?: "",
-                    style = MaterialTheme.typography.bodyLarge,
-                    modifier = Modifier.weight(0.7f),
-                    color = MaterialTheme.colorScheme.onSurface
-                )
-            }
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight()
+                    .padding(spacing.medium)
             ) {
 
-                Text(
-                    text = stringResource(id = R.string.email),
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.name),
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.weight(0.3f),
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+
+                    Text(
+                        text = viewModel?.currentUser?.displayName ?: "",
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.weight(0.7f),
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                }
+
+                Spacer(modifier = Modifier.size(20.dp))
+
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.points),
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.weight(0.3f),
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+
+                    Text(
+                        text = getData.value.points.toString(),
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.weight(0.3f),
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+
+                }
+
+                Spacer(modifier = Modifier.size(20.dp))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+
+                ) {Text(
+                    text = "Bottles",
                     style = MaterialTheme.typography.bodyLarge,
                     modifier = Modifier.weight(0.3f),
                     color = MaterialTheme.colorScheme.onSurface
                 )
 
-                Text(
-                    text = viewModel?.currentUser?.email ?: "",
-                    style = MaterialTheme.typography.bodyLarge,
-                    modifier = Modifier.weight(0.7f),
-                    color = MaterialTheme.colorScheme.onSurface
-                )
-            }
+                    Text(
+                        text = getData.value.bottles.toString(),
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.weight(0.3f),
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                }
 
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight()
-            ) {
-                Text(
-                    text = stringResource(id = R.string.points),
-                    style = MaterialTheme.typography.bodyLarge,
-                    modifier = Modifier.weight(0.3f),
-                    color = MaterialTheme.colorScheme.onSurface
-                )
+                Button(
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(top = spacing.extraLarge),
+                    //enabled = enabledButton() == true,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFF2196F3),
+                        contentColor = Color.White,
+                        disabledContentColor = Color.White
+                    ),
+                    onClick = {
 
-                Text(
-                    text = getData.value.points.toString(),
-                    style = MaterialTheme.typography.bodyLarge,
-                    modifier = Modifier.weight(0.3f),
-                    color = MaterialTheme.colorScheme.onSurface
-                )
-
-            }
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight()
-            ) {Text(
-                text = "Bottles",
-                style = MaterialTheme.typography.bodyLarge,
-                modifier = Modifier.weight(0.3f),
-                color = MaterialTheme.colorScheme.onSurface
-            )
-
-                Text(
-                    text = getData.value.bottles.toString(),
-                    style = MaterialTheme.typography.bodyLarge,
-                    modifier = Modifier.weight(0.3f),
-                    color = MaterialTheme.colorScheme.onSurface
-                )
-            }
-
-            Button(
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .padding(top = spacing.extraLarge),
-                //enabled = enabledButton() == true,
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color(0xFF2196F3),
-                    contentColor = Color.White,
-                    disabledContentColor = Color.White
-                ),
-                onClick = {
-
-                    navController.navigate(Destinations.QrScreen.route) {
-                        popUpTo(Destinations.Profile.route) {
-                            inclusive = true
+                        navController.navigate(Destinations.QrScreen.route) {
+                            popUpTo(Destinations.Profile.route) {
+                                inclusive = true
+                            }
                         }
-                    }
-                },
-                shape = RoundedCornerShape(50),
-            ) {
-                Text(
-                    text = "Generate QR",
-                    style = MaterialTheme.typography.titleSmall
-                )
-            }
+                    },
+                    shape = RoundedCornerShape(50),
+                ) {
+                    Text(
+                        text = "Generate QR",
+                        style = MaterialTheme.typography.titleSmall
+                    )
+                }
 
 
-            Button(
-                onClick =/* onSignOut*/ {
-                    viewModel?.logout()
-                    navController.navigate(Destinations.Login.route) {
-                        popUpTo(Destinations.Profile.route) {
-                            inclusive = true
+                Button(
+                    onClick =/* onSignOut*/ {
+                        viewModel?.logout()
+                        navController.navigate(Destinations.Login.route) {
+                            popUpTo(Destinations.Profile.route) {
+                                inclusive = true
+                            }
                         }
-                    }
-                },
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .padding(top = spacing.extraLarge),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color(0xFF2196F3)
-                )
-            ) {
-                Text(text = stringResource(id = R.string.logout))
+                    },
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(top = spacing.large),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFF2196F3)
+                    )
+                ) {
+                    Text(text = stringResource(id = R.string.logout))
+                }
+
             }
 
         }
+
+
     }
 }
-
 
 /*@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
 @Composable
-fun HomeScreenPreviewLight() {
-    AppRefilTheme {
-        ProfileScreen(
-            null, rememberNavController(),
-            es.refil.presentation.auth.registration.UserData(
-                "51545645645",
-                "prueba@mail.com",
-                "pepito",
-                0,
-                ""
-            )
-        ) {}
-    }
-}
+fun ProfileScreenPreview() {
+    val user = User(
+        uid = "",
+        name = "",
+        email = "",
+        points = 0,
+        bottles = 0,
+        favorites = emptyList()
+    )
 
-@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
-@Composable
-fun HomeScreenPreviewDark() {
     AppRefilTheme {
         ProfileScreen(
-            null, rememberNavController(),
-            es.refil.presentation.auth.registration.UserData(
-                "51545645645",
-                "prueba@mail.com",
-                "pepito",
-                0,
-                ""
-            ),
-        ) {}
+            viewModel = null,
+            userViewModel = hiltViewModel(),
+            navController = rememberNavController(),
+            userData = user,
+            onSignOut = {}
+        )
     }
 }*/
